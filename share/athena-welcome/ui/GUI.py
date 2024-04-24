347a620cc4fe2a45c103703d801ad6468b1b56eb
D3vil0p3r <crazysnob@live.it>
2024-04-24 18:26:46 +0200

    Update logo name

--- share/athena-welcome/ui/GUI.py
+++ share/athena-welcome/ui/GUI.py
@@ -54,7 +54,7 @@ def GUI(self, Gtk, GdkPixbuf):
             headerbar.pack_start(
                 Gtk.Image().new_from_pixbuf(
                     GdkPixbuf.Pixbuf().new_from_file_at_size(
-                        os.path.join(base_dir, "images/athena-logo-white.png"), 16, 16
+                        os.path.join(base_dir, "images/athena.png"), 16, 16
                     )
                 )
             )
